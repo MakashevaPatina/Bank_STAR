82ea99c0112e9817ec55c380f2897fe14a1f72c7
tim40 <tim4054@gmail.com>
2024-11-27 22:49:44 +0300

    ver1.0

--- src/main/java/com/starbank/recommendation_service/rules/RecommendationRuleSet.java
+++ src/main/java/com/starbank/recommendation_service/rules/RecommendationRuleSet.java
@@ -1,7 +1,5 @@
 package com.starbank.recommendation_service.rules;
 
-import java.util.Optional;
-
 public interface RecommendationRuleSet {
     Optional<Recommendation> getRecommendation(Long userId);
 }
