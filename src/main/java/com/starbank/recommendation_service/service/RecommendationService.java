6639c7e015b4ca1f03e3a55581eca220c5ef99a3
Patina Makasheva <patinamakasheva@gmail.com>
2024-11-28 20:25:04 +0300

    Creating a controller, service and DTO

---
+++ src/main/java/com/starbank/recommendation_service/service/RecommendationService.java
@@ -0,0 +1,28 @@
+package com.starbank.recommendation_service.service;
+
+import com.starbank.recommendation_service.dto.RecommendationDTO;
+import com.starbank.recommendation_service.dto.RecommendationResponse;
+import com.starbank.recommendation_service.rules.RecommendationRuleSet;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RecommendationService {
+
+    private final List<RecommendationRuleSet> ruleSets;
+
+    public RecommendationService(List<RecommendationRuleSet> ruleSets) {
+        this.ruleSets = ruleSets;
+    }
+
+
+    public RecommendationResponse getRecommendations(String userId) {
+
+        List<RecommendationDTO> recommendations = ruleSets.stream()
+                .map(rule -> rule.getRecommendation(userId))
+                .collect(Collectors.toList());
+        return new RecommendationResponse(recommendations, userId);
+    }
+}
