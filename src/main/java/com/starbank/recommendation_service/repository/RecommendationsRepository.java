8278c01a68c72645a99d992466b5e2cd2f526169
russa89 <natast2189@gmail.com>
2024-11-28 22:33:21 +0300

    v.1

---
+++ src/main/java/com/starbank/recommendation_service/repository/RecommendationsRepository.java
@@ -0,0 +1,89 @@
+package com.starbank.recommendation_service.repository;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public class RecommendationsRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public RecommendationsRepository(@Qualifier("recommendationsJdbcTemplate") JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int getRandomTransactionAmount(UUID user) {
+        Integer result = jdbcTemplate.queryForObject(
+                "SELECT amount FROM transactions t WHERE t.user_id = ? LIMIT 1",
+                Integer.class,
+                user);
+        return result != null ? result : 0;
+    }
+
+    public int checkDBForProductTypePlusTransactionType(UUID user, String productType, String transactionType) {
+        Integer result = jdbcTemplate.queryForObject(
+                "SELECT amount FROM products p JOIN transactions t ON t.product_id = p.id WHERE user_id = ? AND " +
+                        "p.type = ? AND t.type = ?",
+                Integer.class, user, productType, transactionType);
+        return result != null ? result : 0;
+    }
+
+    public int checkDBProductTypeExcluded(UUID user, String productType) {
+        Integer result = jdbcTemplate.queryForObject(
+                "SELECT amount FROM products p JOIN transactions t ON t.product_id = p.id WHERE user_id = ? AND " +
+                        "p.type != ?",
+                Integer.class, user, productType);
+        return result != null ? result : 0;
+    }
+
+    public Recommendation checkRecommendationInvest500(String id) {
+        Recommendation recommendation = null;
+        UUID uuid = UUID.fromString(id);
+
+        if (checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "DEPOSIT") +
+                checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "WITHDRAW") > 0 &&
+
+                checkDBProductTypeExcluded(uuid, "INVEST") > 0 &&
+
+                checkDBForProductTypePlusTransactionType(uuid, "SAVING", "DEPOSIT") > 1000) {
+
+            recommendation = new Recommendation();
+        }
+        return recommendation;
+    }
+
+    public Recommendation checkRecommendationSimpleCredit(String id) {
+        Recommendation recommendation = null;
+        UUID uuid = UUID.fromString(id);
+
+        if (checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "DEPOSIT") +
+                checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "WITHDRAW") > 0 &&
+
+                (checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "DEPOSIT") >= 50_000 ||
+                        checkDBForProductTypePlusTransactionType(uuid, "SAVING", "DEPOSIT") >= 50_000) &&
+
+                checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "DEPOSIT") >
+                        checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "WITHDRAW")) {
+
+            recommendation = new Recommendation();
+        }
+        return recommendation;
+    }
+    public Recommendation checkRecommendationTopSaving(String id) {
+        Recommendation recommendation = null;
+        UUID uuid = UUID.fromString(id);
+
+        if (checkDBProductTypeExcluded(uuid, "CREDIT") > 0 &&
+
+                checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "DEPOSIT") >
+                        checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "WITHDRAW") &&
+
+                checkDBForProductTypePlusTransactionType(uuid, "DEBIT", "WITHDRAW") > 100_000) {
+
+            recommendation = new Recommendation();
+        }
+        return recommendation;
+    }
+}
