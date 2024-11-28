2204c4d66066f3a97c6881864e6bf0c8bbbce38e
tim40 <tim4054@gmail.com>
2024-11-29 00:57:38 +0300

    ver1.1 changed userId data type to String renamed Recommendation to RecommendationDTO injected repository

--- src/main/java/com/starbank/recommendation_service/rules/RuleInvest500.java
+++ src/main/java/com/starbank/recommendation_service/rules/RuleInvest500.java
@@ -1,5 +1,6 @@
 package com.starbank.recommendation_service.rules;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -7,13 +8,14 @@ import java.util.Optional;
 @Component
 public class RuleInvest500 implements RecommendationRuleSet {
 
-    Repository repository;
+    @Autowired
+    RecommendationsRepository repository;
 
     @Override
-    public Optional<Recommendation> getRecommendation(Long userId) {
+    public Optional<RecommendationDTO> getRecommendation(String userId) {
         // Логика проверки, подходит ли пользователь под специальное предложение, будет зависеть от метода репозитория
         if (repository.checkRecommendtionInvest500(userId) != null) {
-            return Optional.of(new Recommendation("147f6a0f-3b91-413b-ab99-87f081d60d5a",
+            return Optional.of(new RecommendationDTO("147f6a0f-3b91-413b-ab99-87f081d60d5a",
                     "Откройте свой путь к успеху с индивидуальным инвестиционным счетом (ИИС) от нашего банка! " +
                             "Воспользуйтесь налоговыми льготами и начните инвестировать с умом. " +
                             "Пополните счет до конца года и получите выгоду в виде вычета на взнос в следующем налоговом периоде. " +
--- src/main/java/com/starbank/recommendation_service/rules/RuleSimpleCredit.java
+++ src/main/java/com/starbank/recommendation_service/rules/RuleSimpleCredit.java
@@ -1,5 +1,6 @@
 package com.starbank.recommendation_service.rules;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -7,13 +8,14 @@ import java.util.Optional;
 @Component
 public class RuleSimpleCredit implements RecommendationRuleSet {
 
-    Repository repository;
+    @Autowired
+    RecommendationsRepository repository;
 
     @Override
-    public Optional<Recommendation> getRecommendation(Long userId) {
+    public Optional<RecommendationDTO> getRecommendation(String userId) {
         // Логика проверки, подходит ли пользователь под специальное предложение, будет зависеть от метода репозитория
         if (repository.checkRecommendtionSimpleCredit(userId) != null) {
-            return Optional.of(new Recommendation("ab138afb-f3ba-4a93-b74f-0fcee86d447f",
+            return Optional.of(new RecommendationDTO("ab138afb-f3ba-4a93-b74f-0fcee86d447f",
                     "Откройте мир выгодных кредитов с нами!" +
                             "Ищете способ быстро и без лишних хлопот получить нужную сумму? Тогда наш выгодный кредит — именно то, что вам нужно! " +
                             "Мы предлагаем низкие процентные ставки, гибкие условия и индивидуальный подход к каждому клиенту." +
