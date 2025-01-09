bbe106c349cac230ffb3a45d6fc395a8c19acddf
MakashevaPatina <163657228+MakashevaPatina@users.noreply.github.com>
2025-01-09 12:35:20 +0300

    Revert "Develop"

--- src/main/java/com/starbank/recommendation_service/repository/DynamicRulesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.starbank.recommendation_service.repository;
-
-import com.starbank.recommendation_service.rules.dynamic.DynamicRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DynamicRulesRepository extends JpaRepository<DynamicRule, Long> {
-}
